80d3f2a290044a4e84004dc0a8a5947d8730ee41
Christina Schoenrogge <c289s059@ku.edu>
2015-03-05 11:09:11 -0600

    --

--- main.c
+++ main.c
@@ -1,13 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <iostream.h>
 #include <string>
 
 #include "command.h"
 
-extern const CMDSIZE = 15;
+extern const CMDSIZE = 1028;
 extern char *cmdline; //[CMDSIZE];
+
+
+/*
+// A process is a single process
+typedef struct process{
+  //struct process *next;      // next process in pipeline 
+  char **argv;                //for exec 
+  pid_t pid;                   //process ID 
+  char completed;             //true if process has completed 
+  char stopped;               //true if process has stopped 
+  int status;                  //reported status value 
+} process;
+// A job is a pipeline of processes.  
+
+
+*/
+
+/*
+typedef struct job
+{
+  struct job *next;           //next active job 
+  char *command;              //command line, used for messages 
+  process *first_process;     //list of processes in this job 
+  pid_t pgid;                 //process group ID 
+  char notified;              //true if user told about stopped job 
+  struct termios tmodes;      // saved terminal modes 
+  int stdin, stdout, stderr;  // standard i/o channels 
+} job;
+
+//The active jobs are linked into a list.  This is its head.   
+job *first_job = NULL;
+
+*/
+
+
+void parser(){
+	 fgets(cmdline, CMDSIZE, stdin);
+}
+
+
 
 
 int main(int argc, char *argv[], char *envp[])
@@ -17,6 +56,8 @@ int main(int argc, char *argv[], char *envp[])
  *
  */
  
+
+
  
  
  /*	string word;
