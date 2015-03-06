01899fddb8d26e0399b22a35ec0c754174d3de19
Christina Schoenrogge <c289s059@ku.edu>
2015-03-05 18:17:07 -0600

    added stuff to main

--- main.c
+++ main.c
@@ -5,41 +5,60 @@
 #include <dirent.h>
 #include <sys/wait.h>
 
-
+typedef int bool;
+#define true 1
+#define false 0
 //may not need extern
 
-const LISTSIZE = 100; 
+const LISTSIZE = 100;
+const MAXARG = 10; 
 extern const CMDSIZE = 1028;
 extern char *cmdline; //[CMDSIZE];
-int genertID, jobcount;
+int generID, jobcount;
 
 
 /* A process is a single process.  */
-typedef struct job
+typedef struct Job
 {
-  int id, argcount;
-  //char **argv;                /* for exec */
-  pid_t pid;                  /* process ID */
-  //char completed;             /* true if process has completed */
-  int running;
-} job;
+    int id;
+    int argcount;
+ 	char *args[MAX_ARGS];
+    pid_t pid;                  /* process ID */
+    int running;	//for background processes only
+  	char * input;
+	char * output;
+} Job;
 
+void initJob(struct  Job* j)
+{
+		j->input = (char*) malloc(10000);	
+		j->output = (char*) malloc(10000);	
+
+		j->id = generID;
+		generID++;
+		j->argcount = 0;
+		//j->background = false;
+		j->running = false;
+		int i;
+		for(i = 0; i < MAX_ARGS; i++){
+			j->args[i] = (char*) malloc(256);
+		}
+
+}
 
 
 /* The active jobs are linked into a list.  This is its head.   */
-int joblist[LISTSIZE];
+Job jobfore[LISTSIZE];
+Job jobback[LISTSIZE];
 
-int getcmd()
-{
-};
 
-void main(int argc, char **argv, char **envp)
-{
+
+int main(int argc, char **argv, char **envp){
 	
 	
 	getcmd(); //needs to read in input and increment job count
 	while(jobcount > 0){
-		printf("[ --QUASH-- ]");
+		printf("[ --QUASH-- ]
 		//execute job i
 		jobcount -= 1;
 	}
